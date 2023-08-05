3ba7dc3a4d1a2d76910d3846f409838aa99c180f
Andrew Pecyna <pecyna.andrew@gmail.com>
2023-08-05 18:07:34 -0400

    Shopping cart context

--- shopping-cart/src/components/Navbar.tsx
+++ shopping-cart/src/components/Navbar.tsx
@@ -1,7 +1,10 @@
 import { Button, Container, Nav, Navbar as NavbarBs} from "react-bootstrap"
 import { NavLink } from "react-router-dom"
+import { useShoppingCart } from "../context/ShoppingCartContext"
 
 export function Navbar() {
+    const { openCart, cartQuantity } = useShoppingCart()
+
     return (
         <NavbarBs sticky="top" className="bg-white shadow-sm mb-3">
             <Container>
@@ -10,9 +13,11 @@ export function Navbar() {
                     <Nav.Link to="/store" as={NavLink}>Store</Nav.Link>
                     <Nav.Link to="/about" as={NavLink}>About</Nav.Link>
                 </Nav>
-                <Button style={{width: "3rem", height: "3rem", position: "relative"}}
-                        variant="outline-primary"
-                        className="rounded-circle">
+                {cartQuantity > 0 && <Button 
+                    onClick={openCart}
+                    style={{width: "3rem", height: "3rem", position: "relative"}}
+                    variant="outline-primary"
+                    className="rounded-circle">
                     <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 576 512"
@@ -31,8 +36,8 @@ export function Navbar() {
                         right: 0,
                         transform: "translate(35%, 25%)"
                     }}
-                    >0</div>
-                </Button>
+                    >{cartQuantity}</div>
+                </Button>}
             </Container>
         </NavbarBs>
     )
